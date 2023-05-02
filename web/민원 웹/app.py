162aeeab6e5541c5ffb6d59f78edc49569b08a74
Geunyoung Kim <dea314@naver.com>
2023-05-02 19:57:49 +0900

    웹 자치구에 맞게 모델 적용

--- web/민원 웹/app.py
+++ web/민원 웹/app.py
@@ -8,7 +8,16 @@ import numpy as np
 app = Flask(__name__)
 
 # 모델 로드
-model = joblib.load('model.joblib')
+BukGu_model = joblib.load('BukGu_model.joblib')
+NamGu_model = joblib.load('NamGu_model.joblib')
+DalseongGun_model = joblib.load('DalseongGun_model.joblib')
+DalsuGu_model = joblib.load('DalsuGu_model.joblib')
+JungGu_model = joblib.load('JungGu_model.joblib')
+SeoGu_model = joblib.load('SeoGu_model.joblib')
+Suseonggu_model = joblib.load('Suseonggu_model.joblib')
+DongGu_model = joblib.load('DongGu_model.joblib')
+
+wv_model = joblib.load('new_wv_model.joblib')
 
 # 라우트
 @app.route('/')
@@ -20,6 +29,30 @@ def predict():
     # 사용자 입력 받기
     sentence = request.form['sentence']
     text = sentence
+    
+    subscription = request.form.get('subscription')
+    
+    # 구별로 다른 모델 사용하기 위한 코드
+    if subscription == 'BukGu':
+        model = BukGu_model
+    elif subscription == 'NamGu':
+        model = NamGu_model
+    elif subscription == 'DalseongGun':
+        model = DalseongGun_model
+    elif subscription == 'DalsuGu':
+        model = DalsuGu_model
+    elif subscription == 'ungGu':
+        model = JungGu_model
+    elif subscription == 'SeoGu':
+        model = SeoGu_model
+    elif subscription == 'Suseonggu':
+        model = Suseonggu_model
+    elif subscription == 'DongGu':
+        model = DongGu_model
+    else:
+        # subscription이 존재하지 않는 경우 기본값인 BukGu로 설정
+        model = BukGu_model
+        
     # 형태소 분석 후 토큰화
     kiwi = Kiwi()
     sentence = kiwi.tokenize(sentence)
@@ -30,9 +63,6 @@ def predict():
     
     # 리스트를 numpy 배열로 변환
     sentence_token = np.array(sentence_token).tolist()
-    
-    # 벡터화
-    wv_model = Word2Vec(sentences=[sentence_token], vector_size=1000, window=5, min_count=5, workers=4, sg=1)
     
     def get_sent_embedding(model, embedding_size, tokenized_words):
         feature_vec = np.zeros((embedding_size,), dtype="float32")
@@ -53,20 +83,34 @@ def predict():
         
         # 각 문장을 벡터화해서 리스트에 저장
         for sent in sentences:
-            dataset.append(get_sent_embedding(model, num_features, sent))
-        
+            sent_embedding_vectors = get_sent_embedding(model, num_features, sent)
+            dataset.append(sent_embedding_vectors)  
+              
         # 리스트를 numpy 배열로 변환하여 반환
         sent_embedding_vectors = np.stack(dataset)
         
         return sent_embedding_vectors
     
-    sentence_token_vc = get_dataset([sentence_token], wv_model, 1000)
+    sentence_token_vc = get_dataset([sentence_token], wv_model, 1400)
     
     # 모델로 예측하기
     category = model.predict(sentence_token_vc)[0]
     
-    # 결과 리턴하기
-    return render_template('result.html', category=category, text=text)
+    # 예측 확률
+    pred = model.predict_proba(sentence_token_vc)[0].max()
+    
+    # 결과 result.html 페이지로 보내기
+    return render_template('result.html', category=category, text=text, pred=pred)
+
+
+@app.route('/predict/manager', methods=['GET', 'POST'])
+def manager():
+    if request.method == 'POST':
+        text = request.form['text']
+        category = request.form['category']
+        pred = request.form['pred']
+        return render_template('manager.html', text=text, category=category, pred=pred)
+
 
 # 앱 실행
 if __name__ == '__main__':
